b7aee7255b07e9000f6b2bcb8072ff0fb201bf65
Ryan <ryankanno@localkinegrinds.com>
2015-09-20 08:48:47 -1000

    Added in start of gtfs manager/model structure

---
+++ allbus/thebus/gtfs_thebus_managers.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.db.models import Q
+import datetime
+import operator
+import pytz
+
+
+class TheBusTripManager(models.Manager):
+
+    DIRECTION = {"west": 0, "east": 1}
+
+    def get_trips(
+            self,
+            route_shortname,
+            direction,
+            utc_dt=None,
+            local_timezone=None):
+        """ Returns the trips for a route given a direction and time. Useful to
+        determine what stops a bus will be stopping at in a given direction on
+        a certain date/time. Does not take into exception schedules at this
+        point, but probably should. """
+        utc_dt = utc_dt or datetime.datetime.utcnow()
+        local_timezone = local_timezone or 'Pacific/Honolulu'
+
+        local_tz = pytz.timezone(local_timezone)
+        local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+        local_time = local_tz.normalize(local_dt)
+        local_day_lower = local_time.strftime('%A').lower()
+
+        predicates = [
+            ('route__short_name', route_shortname),
+            ('service__start_date__lt', local_time),
+            ('service__end_date__gt', local_time),
+            ('direction', self.DIRECTION[direction.lower()]),
+            ("service__{0}".format(local_day_lower), True)
+        ]
+
+        q_list = [Q(x) for x in predicates]
+        return self.filter(reduce(operator.and_, q_list))
+
+# vim: filetype=python
---
+++ allbus/thebus/gtfs_thebus_models.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from gtfs_thebus_managers import TheBusTripManager
+from multigtfs.models import Trip
+
+
+class TheBusTrip(Trip):
+
+    class Meta:
+        proxy = True
+
+    objects = TheBusTripManager()
+
+    def __init__(self, *args, **kwargs):
+        super(TheBusTrip, self).__init__(*args, **kwargs)
+
+    def get_stop_times(self):
+        return [x for x in self.stoptime_set.all()]
+
+    def get_stops(self):
+        return [x.stop for x in self.stoptime_set.all().only('stop')]
+
+    def get_previous_stop(self, stop_id):
+        stop_id_index = self._get_index_of_stop_id_in_trip(stop_id)
+        if stop_id_index is not None and stop_id_index != 0:
+            return list(self.stoptime_set.all())[stop_id_index - 1].stop
+        else:
+            return None
+
+    def get_next_stop(self, stop_id):
+        stop_id_index = self._get_index_of_stop_id_in_trip(stop_id)
+        if stop_id_index is not None \
+                and stop_id_index != self.stoptime_set.all().count() - 1:
+            return list(self.stoptime_set.all())[stop_id_index + 1].stop
+        else:
+            return None
+
+    def _get_index_of_stop_id_in_trip(self, stop_id):
+        stop_id_index = [
+            i for i, stop_time in enumerate(self.stoptime_set.all())
+            if stop_time.stop.stop_id == stop_id]
+
+        if len(stop_id_index) == 1:
+            return stop_id_index[0]
+        else:
+            return None
+
+# vim: filetype=python
