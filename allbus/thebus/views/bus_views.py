1ffd46e71fee32c6076f1bb9806a0dafd6239c78
Ryan <ryankanno@localkinegrinds.com>
2016-04-26 17:16:25 +0000

    Added thebus views

---
+++ allbus/thebus/views/bus_views.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+from django.conf import settings
+from django.http import Http404
+from django.http import HttpResponse
+
+import json
+from ..utilities.client import parse_xml_to_dict
+from ..utilities.client import TheBusClient
+from ..utilities.time import naive_to_timestamp
+
+
+def bus_details(request, bus):
+    c = TheBusClient(settings.THEBUS_API_CLIENT_TOKEN)
+    v_json = c.track_vehicle(int(bus), parse_xml_to_dict)
+    vehicles = v_json.get('vehicles', None)
+    vehicle = vehicles.get('vehicle', None)
+
+    if vehicle:
+        if 'last_message' in vehicle:
+            naive_datetime = datetime.datetime.strptime(
+                vehicle['last_message'],
+                "%m/%d/%Y %I:%M:%S %p")
+            vehicle['last_message_ts'] = naive_to_timestamp(
+                naive_datetime, tz='Pacific/Honolulu')
+
+        return HttpResponse(json.dumps(vehicle), mimetype="application/json")
+    else:
+        raise Http404("Unable to find Bus {}".format(str(bus)))
+
+# vim: filetype=python
---
+++ allbus/thebus/views/trip_views.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+import json
+from ..models import TheBusTrip
+
+
+def trip_details(request, trip_id):
+    t = get_object_or_404(TheBusTrip, trip_id=trip_id)
+    return HttpResponse(json.dumps(t.to_dict()), mimetype="application/json")
+
+# vim: filetype=python
