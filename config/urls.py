c787a4ad0d7cb877fa84223030eb229810df2330
Ryan <ryankanno@localkinegrinds.com>
2016-04-26 17:18:14 +0000

    Configured app urls

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,37 @@
-"""allbus URL Configuration
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 from django.contrib import admin
 
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    # Allbus urls
+    url(r'^', include("allbus.thebus.urls")),
+
+    # Django Admin
+    url(r'^admin/', include(admin.site.urls)),
+
+    # GTFS Explorer
+    url(r'^explore$', TemplateView.as_view(template_name="explore/home.html"),
+        name='home'),
+    url(r'^explore/', include('allbus.exploreapp.urls'))
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls))
+    ]
+    urlpatterns += [
+        url(r'^400/$', 'django.views.defaults.bad_request'),
+        url(r'^403/$', 'django.views.defaults.permission_denied'),
+        url(r'^404/$', 'django.views.defaults.page_not_found'),
+        url(r'^500/$', 'django.views.defaults.server_error'),
+    ]
