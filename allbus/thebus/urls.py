42020ede9fb176bc99388d864c609bf0e2a7b38c
Ryan <ryankanno@localkinegrinds.com>
2016-04-26 18:13:34 +0000

    Made end slash optional

--- allbus/thebus/urls.py
+++ allbus/thebus/urls.py
@@ -10,23 +10,23 @@ from .views import trip_views
 
 urlpatterns = [
     # stop views
-    url(r'^stops/$', stop_views.stop_search, name="stop_search"),
+    url(r'^stops/?$', stop_views.stop_search, name="stop_search"),
 
-    url(r'^(?P<stop_id>\d+)/$', stop_views.stop_details, {'route': None},
+    url(r'^(?P<stop_id>\d+)/?$', stop_views.stop_details, {'route': None},
         name="stop_details_no_route"),
 
-    url(r'^(?P<stop_id>\d+):(?P<route>[a-zA-Z0-9]+)/$',
+    url(r'^(?P<stop_id>\d+):(?P<route>[a-zA-Z0-9]+)/?$',
         stop_views.stop_details,
         name="stop_details"),
 
-    url(r'^es/near/(?P<latitude>[\-\.0-9]+)/(?P<longitude>[\-\.0-9]+)/$',
+    url(r'^es/near/(?P<latitude>[\-\.0-9]+)/(?P<longitude>[\-\.0-9]+)/?$',
         stop_views.stop_nearby, name="stop_nearby"),
 
     # bus views
-    url(r'^buses/(?P<bus>[\d]+)$', bus_views.bus_details, name="bus_details"),
+    url(r'^buses/(?P<bus>[\d]+)/?$', bus_views.bus_details, name="bus_details"),
 
     # trip views
-    url(r'^trips/(?P<trip_id>[\d\.]+)$',
+    url(r'^trips/(?P<trip_id>[\d\.]+)/?$',
         trip_views.trip_details,
         name="trip_details"),
 ]
