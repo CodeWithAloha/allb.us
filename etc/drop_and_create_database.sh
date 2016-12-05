3516397a108f6b22ad81e2a65a47bba35c0a94e8
Ryan <ryankanno@localkinegrinds.com>
2016-12-05 09:27:30 -1000

    Fixed script to use same name as database in production

--- etc/drop_and_create_database.sh
+++ etc/drop_and_create_database.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo -u postgres dropdb allbus
-sudo -u postgres createdb allbus -O allbus
-sudo -u postgres psql -c "CREATE EXTENSION postgis;" allbus
+sudo -u postgres dropdb allbus_db
+sudo -u postgres createdb allbus_db -O allbus
+sudo -u postgres psql -c "CREATE EXTENSION postgis;" allbus_db
