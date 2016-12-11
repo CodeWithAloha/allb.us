ea0dba00d4cf26a88b96caf6ebc862395a282704
Ryan <ryankanno@localkinegrinds.com>
2016-12-10 18:08:47 -1000

    Added Your Favs header

--- allbus-frontend/src/components/FavoritesListComponent.vue
+++ allbus-frontend/src/components/FavoritesListComponent.vue
@@ -3,6 +3,7 @@
     <p> You have no favorite stops </p>
   </div>
   <div v-else>
+    <h1>Your Favs</h1>
     <ul>
       <li v-for="(fav, index) in favorites" class="favorite">
         <router-link :to="{ name: 'stopDetails', params: { stopId: fav.stopId }}">
