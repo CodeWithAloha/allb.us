f8c44e0b6a9004362385c3be3169bf419bb33a55
Ryan <ryankanno@localkinegrinds.com>
2016-12-13 20:14:09 -1000

    Switched mutation - stop was missing the point object

--- allbus-frontend/src/store/mutations.js
+++ allbus-frontend/src/store/mutations.js
@@ -11,10 +11,15 @@ export default {
     state.stop = newStop
   },
   [types.GET_STOP_BUS_MAP] (state, { stopBusMap }) {
-    state.stop.stop_id = stopBusMap.stop.stop_id
-    state.stop.name = stopBusMap.stop.name
-    state.stop.point.x = stopBusMap.stop.longitude
-    state.stop.point.y = stopBusMap.stop.latitude
+    var newStop = {
+      'stop_id': stopBusMap.stop.stop_id,
+      'name': stopBusMap.stop.name,
+      'point': {
+        'x': stopBusMap.stop.longitude,
+        'y': stopBusMap.stop.latitude
+      }
+    }
+    state.stop = newStop
     state.route.stops = stopBusMap.stops
     state.trip = stopBusMap.trip
     state.vehicle = stopBusMap.vehicle
