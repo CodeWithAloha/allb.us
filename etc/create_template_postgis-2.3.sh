f039b58796af409a495cfc64b5b2fdd769bc9b20
Ryan <ryankanno@localkinegrinds.com>
2017-01-04 17:27:21 -1000

    Renamed create_template_postgis-2.2.sh to create_template_postgis-2.3.sh. Updated script to use postgis 2.3

--- etc/create_template_postgis-2.3.sh
+++ etc/create_template_postgis-2.3.sh
@@ -4,11 +4,12 @@ if [[ `uname -r | grep el6` ]]; then
   POSTGIS_SQL_PATH=`pg_config --sharedir`/contrib/postgis
   POSTGIS_SQL_FILE=$POSTGIS_SQL_PATH/postgis-64.sql
 else
-  POSTGIS_SQL_PATH=`pg_config --sharedir`/contrib/postgis-2.2
+  POSTGIS_SQL_PATH=`pg_config --sharedir`/contrib/postgis-2.3
   POSTGIS_SQL_FILE=$POSTGIS_SQL_PATH/postgis.sql
 fi
 
-createdb -E UTF8 -T template0 --locale=en_US.utf8 template_postgis # Create the template spatial database.
+createdb -E UTF8 -T template0 --locale=en_US.UTF-8 template_postgis # Create the template spatial database.
+
 # createlang -d template_postgis plpgsql # Adding PLPGSQL language support.
 psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
 psql -d template_postgis -f $POSTGIS_SQL_FILE # Loading the PostGIS SQL routines
